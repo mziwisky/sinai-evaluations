8705752abaa48762878519b4a8b561641828f0e5
Michael Ziwisky <mziwisky@instructure.com>
2015-09-10 00:45:12 -0500

    student form validations and beautifications

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,28 +1,46 @@
 class EvaluationsController < ApplicationController
   def show_student
     @evaluation = Evaluation.where(student_access_code: params[:access_code]).first or not_found
-    @providers = Provider.where(disabled: false)
-    @student_types = [
-      ['M3', 'M3'],
-      ['M4', 'M4'],
-      ['Physician Assistant', 'PA']]
-    @hospitals = [
-      ['Mount Sinai', 'Mount Sinai'],
-      ['Holy Cross', 'Holy Cross'],
-    ]
+    prepare_student_form_vars
+  end
+
+  def student_submit
+    @evaluation = Evaluation.where(student_access_code: params[:access_code]).first or not_found
+    @evaluation.student_update(params[:evaluation])
+
+    if @evaluation.valid?
+      # TODO: redirect to a view page or something
+      render text: "thanks! -- #{params[:evaluation].to_json}"
+    else
+      prepare_student_form_vars
+      render 'show_student'
+    end
   end
 
   def show_provider
     @evaluation = Evaluation.where(provider_access_code: params[:access_code]).first or not_found
   end
 
-  def student_submit
-    @evaluation = Evaluation.where(student_access_code: params[:access_code]).first or not_found
-    @evaluation.update_attributes(params[:evaluation])
-    render text: params[:evaluation].to_json
+  def provider_submit
+    # TODO:
   end
 
   private
+
+  def prepare_student_form_vars
+    @providers = Provider.where(disabled: false)
+    @student_types = [
+      ['M3', 'M3'],
+      ['M4', 'M4'],
+      ['Physician Assistant', 'PA']]
+    @hospitals = [
+      ['Condell Hospital', 'Condell'],
+      ['Stroger Hospital', 'Stroger'],
+      ['Ross Medical School', 'Ross'],
+      ['Mount Sinai Hospital', 'Mount Sinai'],
+      ['Holy Cross Hospital', 'Holy Cross'],
+    ]
+  end
 
   def not_found
     raise ActiveRecord::RecordNotFound
--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,11 +2,24 @@ class Evaluation < ActiveRecord::Base
   attr_accessible :student_email, :student_name, :student_type, :hospital, :evaluation, :provider_id
   belongs_to :provider
 
+  attr_accessor :student_submission, :evaluator_submission
   validates_presence_of :student_email, :student_name
+  validates_presence_of :student_type, :hospital, :provider_id, :if => lambda { self.student_submission }
+  validates_presence_of :evaluation, :if => lambda { self.evaluator_submission }
 
   before_create :generate_access_codes
 
   after_create :send_email_to_student
+
+  def student_update(attrs)
+    self.student_submission = true
+    update_attributes(attrs)
+  end
+
+  def evaluator_update(attrs)
+    self.evaluator_submission = true
+    update_attributes(attrs)
+  end
 
   private
 
