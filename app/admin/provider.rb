a96a540a0d28a213ecc2d3dbf80454b3a0ad5a5e
Michael Ziwisky <mziwisky@instructure.com>
2015-09-14 14:52:53 -0500

    add scopes to providers index

--- app/admin/provider.rb
+++ app/admin/provider.rb
@@ -1,11 +1,14 @@
 ActiveAdmin.register Provider do
+  scope :active, default: true
+  scope :disabled
+  scope :all
 
   form do |f|
     f.semantic_errors
     f.inputs do
       f.input :name, as: :string
       f.input :email, as: :email
-      f.input :disabled, label: 'Disabled (disabled providers cannot be selected by students to do evaluations)'
+      f.input :disabled, hint: 'disabled providers cannot be selected by students to do evaluations'
     end
     f.actions
   end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -2,4 +2,7 @@ class Provider < ActiveRecord::Base
   attr_accessible :email, :name, :disabled
 
   validates_presence_of :email, :name
+
+  scope :active, lambda { where(disabled: [false, nil]) }
+  scope :disabled, lambda { where(disabled: true) }
 end
